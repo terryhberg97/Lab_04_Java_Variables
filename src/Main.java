46b67e55d9b90a5b242020c6896eaf6348e33dd6
terryhberg97 <terryhberg97@gmail.com>
2026-02-09 18:43:21 -0500

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        String test = "test";
+        String test = "tests";
         double myLunchCost = 12.50;
         int numberOfKids = 0;
         boolean isItRaining = false;
