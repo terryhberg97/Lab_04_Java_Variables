b77431ba844f67bf4f1db7136b8fe3bf3e2cba2d
terryhberg97 <terryhberg97@gmail.com>
2026-02-09 18:14:36 -0500

    Init - Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         int birthMonth = 4;
         String fullName = "Tyler Humfleet";
 
-
+        //TIP
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         System.out.println("The cost of my lunch is: " + myLunchCost);
