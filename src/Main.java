5b6953e95916eb895f2d706151dc276f7c73a508
terryhberg97 <terryhberg97@gmail.com>
2026-02-09 18:01:33 -0500

    Init

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        double myLunchCost = 12.50;
+        int numberOfKids = 0;
+        boolean isItRaining = false;
+        double gallonGasPrice = 2.29;
+        int favoriteNumber = 27;
+        int shoeSize = 12;
+        int birthMonth = 4;
+        String fullName = "Tyler Humfleet";
+
+
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        System.out.println("The cost of my lunch is: " + myLunchCost);
+    }
+}
+
